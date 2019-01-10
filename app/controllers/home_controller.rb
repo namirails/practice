39f133efcadd7be6ce9b49516424700687c80637
nami-practice <test@example.com>
2019-01-10 06:59:42 +0000

    bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @hello = "Hello"
+    @clips = Clip.limit(8).order(created_at: :desc)
+    # descは降順。大きい方から並べること
+    # 例）5, 4, 3, 2, 1
+    # 日付は新しいものほど大きい
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :categories
   resources :user_clips
-  resources :clips
+  resources :clips, only: [:index, :show]
   resources :articles
   devise_for :users
   get 'home/index'
-  root 'articles#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
